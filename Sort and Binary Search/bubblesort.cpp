7e26fe5b2102c27a42d8be8d335c15352516c0ca
Lucas-Mombach <lucasmmombach@gmail.com>
2023-04-25 16:59:16 -0300

    bubblesort file add

---
+++ Sort and Binary Search/bubblesort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+void sortationMethod(int *array, int n){
+//cool implementation here
+}
+//same main for every sortation method
+int main(){
+  int n = 5;
+  int *array = new int(n);
+  array[0] = 5;
+  array[1] = 2;
+  array[2] = 3;
+  array[3] = 8;
+  array[4] = 1;
+  std::cout << "Unsorted array: " << std::endl;
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << array[i] << " ";
+  }
+
+  sortationMethod(array,n);
+
+  std::cout << "\nSorted array: " << std::endl;
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << array[i] << " ";
+  }
+  
+}
